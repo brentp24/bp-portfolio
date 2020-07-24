faba86b5a01d03a1c9c9506f1bcdea1d9c811a37
Brent Peterson <brentp24@gmail.com>
2020-07-24 09:36:52 -0600

    navbar

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,44 +1,106 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { Component } from "react";
+import { NavLink } from "react-router-dom";
 import "./style.css";
+class NavBar extends Component {
 
-// Depending on the current path, this component sets the "active" class on the appropriate navigation link item
-function NavBar() {
-    return (
-        <nav className="navbar navbar-expand-md bg-light">
-            <Link className="navbar-collapse collapse w-100 order-1 order-md-0 dual-collapse2" to="/">
-                Brent Peterson
-            </Link>
-            <div>
-                <ul className="navbar-nav mr-auto">
-                    <li className="nav-item">
-                        <Link
-                            to="/"
-                            className={window.location.pathname === "../../pages/Login.js" ? "nav-link active" : "nav-link"}
-                        >
-                            About
+    render() {
+        return (
+            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                <h3>
+                    <NavLink to="/"
+                        className="navbar-brand">
+                        Brent Peterson
+            </NavLink>
+                </h3>
+                {/* <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button> */}
 
-                        </Link>
+                <div className="collapse navbar-collapse" id="navbarNavDropdown">
+                    <h5>
+                        <ul className="navbar-nav">
+                            <li className="nav-item">
+                                <NavLink
+                                    to="/"
+                                    // className="navbar-collapse collapse w-100 order-1 order-md-0 dual-collapse2"
+                                    style={{ color: "darkGray" }}
+                                    activeStyle={{ color: "black" }}
+                                    exact
+                                >
+                                    About
+                        </NavLink>
+                            </li>
+                            <li className="nav-item">
+                                <NavLink
+                                    to="/portfolio"
+                                    // className="navbar-collapse collapse w-100 order-1 order-md-0 dual-collapse2"
+                                    style={{ color: "darkGray" }}
+                                    activeStyle={{ color: "black" }}
+                                    exact
+                                >
+                                    Portfolio
+                        </NavLink>
+                            </li>
 
-    
-                    </li>
-                </ul>
-                </div>
+
+                        </ul>
+                    </h5>
+                    {/* </div>
                 <div className="navbar-collapse collapse w-100 order-3 dual-collapse2">
                 <ul className="navbar-nav ml-auto">
-                    <li className="nav-item">
-                        <Link
-                            to="/portfolio"
-                            className={window.location.pathname === "../../pages/Login.js" ? "nav-link active" : "nav-link"}
-                        >
-                            Portfolio
-                        </Link>
-                    </li>
-                </ul>
+                    
+                </ul> */}
 
-            </div>
-        </nav>
-    );
+                </div>
+            </nav>
+        )
+
+    }
 }
+
+
+
+// // Depending on the current path, this component sets the "active" class on the appropriate navigation link item
+// function NavBar() {
+//     return (
+//         <nav className="navbar navbar-expand-md bg-light">
+//             <h4>
+//             <Link to="/">
+//                 Brent Peterson
+//             </Link>
+//             </h4>
+//             <div>
+//                 <ul className="navbar-nav mr-auto">
+//                     <h5>
+//                     <li className="nav-item">
+//                         <Link
+//                             to="/"
+//                             activeStyle={{color: "black"}}
+//                         >
+//                             About
+//                         </Link>
+//                     </li>
+//                     <li className="nav-item">
+//                         <Link
+//                             to="/portfolio" 
+//                             activeStyle={{color: "black"}}
+//                             exact
+//                         >
+//                             Portfolio
+//                         </Link>
+//                     </li>
+//                     </h5>
+
+//                 </ul>
+//                 {/* </div>
+//                 <div className="navbar-collapse collapse w-100 order-3 dual-collapse2">
+//                 <ul className="navbar-nav ml-auto">
+
+//                 </ul> */}
+
+//             </div>
+//         </nav>
+//     );
+// }
 
 export default NavBar;
